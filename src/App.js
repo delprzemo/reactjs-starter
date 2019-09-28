e624f49aaba7f945858f8bba8ed39f391e417396
delprzemo <delprzemo@gmail.com>
2019-09-28 12:06:19 +0200

    Exercise 1 - red/green object depended on seconds

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
 
 function App() {
+  let colorClassName = "red-background";
+
+  function getTickElement() {
+    var second = new Date().getSeconds();
+    colorClassName = (second % 2 === 0) ? "green-background" : "red-background";
+    const element = <div className={colorClassName}></div>
+    return element;
+  }
+
   return (
-    <div className="App">
-      Hello world
-    </div>
+    getTickElement()
   );
 }
 
