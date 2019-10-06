4ba55c391f7fa5212010633536e220893cf912a3
delprzemo <delprzemo@gmail.com>
2019-10-06 08:41:59 +0200

    Exercise 1a - component lazy loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import Menu from './Menu/Menu'
-import Help from './Help/Help'
 import Home from './Home/Home'
 import List from './List/List'
 import './App.css';
@@ -9,19 +8,25 @@ import NoAccess from './NoAccess/NoAccess';
 import PrivateRoute from './Common/PrivateRoute/PrivateRoute';
 
 
+const Help = lazy(() => import('./Help/Help'));
+
+
 class App extends React.Component {
   render() {
     return (
       <div>
         <Router>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+              <Route path="/help/:id" component={Help}></Route>
+              <PrivateRoute path="/list" component={List}></PrivateRoute>
+              <Route path="/noAccess"><NoAccess /></Route>
+              <Route path="/"><Home /></Route>
+            </Switch>
+          </Suspense>
           <Menu />
 
-          <Switch>
-            <Route path="/help/:id" component={Help}></Route>
-            <PrivateRoute path="/list" component={List}></PrivateRoute>
-            <Route path="/noAccess"><NoAccess /></Route>
-            <Route path="/"><Home /></Route>
-          </Switch>
+
         </Router>
       </div>
     );
